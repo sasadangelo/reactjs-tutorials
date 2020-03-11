3040ab0abd3f3d37667e745c7c611f5de61da5f1
sasadangelo <sasadangelo@gmail.com>
2020-03-11 10:45:25 +0100

    Added 3 tutorials: menu, hello-react, first-react-app

---
+++ menu/src/App.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import clsx from 'clsx'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Drawer from '@material-ui/core/Drawer'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+
+import AppMenu from './components/AppMenu'
+
+const PageDashboard = () => <Typography variant="h3" component="h1">Dashboard Page</Typography>
+const PageOrders = () => <Typography variant="h3" component="h1">Orders Page</Typography>
+const PageCustomers = () => <Typography variant="h3" component="h1">Customers Page</Typography>
+const PageReports = () => <Typography variant="h3" component="h1">Reports Page</Typography>
+
+const App: React.FC = () => {
+  const classes = useStyles()
+
+  return (
+    <BrowserRouter>
+      <div className={clsx('App', classes.root)}>
+        <CssBaseline />
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <AppMenu />
+        </Drawer>
+        <main className={classes.content}>
+          <Container maxWidth="lg" className={classes.container}>
+
+            <Switch>
+              <Route path="/" exact component={PageDashboard} />
+              <Route path="/orders" component={PageOrders} />
+              <Route path="/customers" component={PageCustomers} />
+              <Route path="/reports" component={PageReports} />
+            </Switch>
+
+          </Container>
+        </main>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+const drawerWidth = 240
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    background: '#535454',
+    color: '#fff',
+  },
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+}))
+
+export default App
